8bcf94ae576c83c53fbd0c5d95aa54da85097681
iaex-2git <imarev_ex@yahoo.com>
2025-12-10 16:27:47 +0600

    3

---
+++ includes/header.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en" data-bs-theme="light">
+
+<head>
+  <title>Bitrader - Professional Multipurpose HTML Template for Your Crypto, Forex, Stocks & Day Trading Business </title>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+  <!-- Sites meta Data -->
+  <meta name="application-name"
+    content="Bitrader - Professional Multipurpose HTML Template for Your Crypto, Forex, Stocks & Day Trading Business">
+  <meta name="author" content="thetork">
+  <meta name="keywords" content="Bitrader, Crypto, Forex, and Stocks Trading Business">
+  <meta name="description"
+    content="Experience the power of Bitrader, the ultimate HTML template designed to transform your trading business. With its sleek design and advanced features, Bitrader empowers you to showcase your expertise, engage clients, and dominate the markets. Elevate your online presence and unlock new trading possibilities with Bitrader.">
+
+  <!-- OG meta data -->
+  <meta property="og:title"
+    content="Bitrader - Professional Multipurpose HTML Template for Your Crypto, Forex, Stocks & Day Trading Business">
+  <meta property="og:site_name" content=Bitrader>
+  <meta property="og:url" content="https://thetork.com/demos/html/bitrader/">
+  <meta property="og:description"
+    content="Welcome to Bitrader, the game-changing HTML template meticulously crafted to revolutionize your trading business. With its sleek and modern design, Bitrader provides a cutting-edge platform to showcase your expertise, attract clients, and stay ahead in the competitive trading markets.">
+  <meta property="og:type" content="website">
+  <meta property="og:image" content="assets/images/og.png">
+
+
+
+  <link rel="shortcut icon" href="assets/images/favicon.png" type="image/x-icon">
+
+  <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+  <link rel="stylesheet" href="assets/css/aos.css">
+  <link rel="stylesheet" href="assets/css/all.min.css">
+
+  <link rel="stylesheet" href="assets/css/swiper-bundle.min.css">
+
+
+
+  <!-- main css for template -->
+  <link rel="stylesheet" href="assets/css/style.css">
+</head>
